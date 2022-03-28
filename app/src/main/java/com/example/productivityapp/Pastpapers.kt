976ba0154d495b01a6dc9371b00ded841da8e526
thushanisuresh <thushanisuresh66@gmail.com>
2022-03-28 12:43:06 +0530

    Implemented pastpaper screen

---
+++ app/src/main/java/com/example/productivityapp/Pastpapers.kt
@@ -0,0 +1,11 @@
+package com.example.productivityapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pastpapers : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pastpapers)
+    }
+}
---
+++ app/src/main/java/com/example/productivityapp/syllabus_view.kt
@@ -0,0 +1,11 @@
+package com.example.productivityapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class syllabus_view : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_syllabus_view2)
+    }
+}
